9e456328fae64c23495aaa2ecbdba6d26190b65f
sansai-SNCT <syunzo1115@icloud.com>
2022-06-20 23:03:18 +0900

    [Update] colorの設定

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ['./src/pages/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: '#434343',
+        background: '#FBFBFB',
+        black: '#1C1C1C',
+        sea: '#45A3CC',
+      },
+    },
   },
   plugins: [],
 }
