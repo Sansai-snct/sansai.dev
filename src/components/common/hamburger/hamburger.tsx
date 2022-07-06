cc6fc49a71bf59dbbc15bc42c93a9c3ee7f23691
sansai-SNCT <syunzo1115@icloud.com>
2022-07-07 00:09:50 +0900

    [Update] jsonからデータを取り出す方式に変更する

--- src/components/common/hamburger/hamburger.tsx
+++ src/components/common/hamburger/hamburger.tsx
@@ -1,6 +1,28 @@
+import * as fs from 'fs'
+import type { GetStaticProps } from 'next'
 import Link from 'next/link'
+import * as path from 'path'
 
-export const Menu: React.FC = () => {
+type Menu = {
+  name: string
+  link: string
+}
+
+type Props = {
+  data: Menu[]
+}
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const jsonPath = path.join(process.cwd(), 'src', 'data', 'data.json')
+  const jsonText = fs.readFileSync(jsonPath, 'utf-8')
+  const data = JSON.parse(jsonText) as Menu[]
+  return {
+    props: { data },
+  }
+}
+const Menu: React.FC<Props> = ({ data }: Props) => {
+  console.log(data)
+
   return (
     <div>
       <ul>
