62a1709831b34117b240194baad6045d8308682e
sansai-SNCT <syunzo1115@icloud.com>
2022-07-08 22:27:05 +0900

    [Update] タグを変更

--- src/components/common/hamburger/hamburger.tsx
+++ src/components/common/hamburger/hamburger.tsx
@@ -11,32 +11,34 @@ export const Menu: FC = () => {
   }
 
   return (
-    <div className="">
-      <button
-        className=""
-        onClick={() => {
-          return handleChange()
-        }}
-      >
-        ボタン
-      </button>
-      <ul>
-        <li>
-          <Link href="#">
-            <a>About</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="#">
-            <a>Works</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="#">
-            <a>Posts</a>
-          </Link>
-        </li>
-      </ul>
-    </div>
+    <nav className="flex">
+      <div className="">
+        <button
+          className="bg-gray-400"
+          onClick={() => {
+            return handleChange()
+          }}
+        >
+          ボタン
+        </button>
+        <ul className="md:hidden">
+          <li>
+            <Link href="#">
+              <a>About</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="#">
+              <a>Works</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="#">
+              <a>Posts</a>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
   )
 }
