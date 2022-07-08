a251105ec6ca52a9e940743f425890f78304ab89
sansai-SNCT <syunzo1115@icloud.com>
2022-07-08 22:12:31 +0900

    [Update] tailwindの適用範囲の拡大

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/pages/*.{js,ts,jsx,tsx}'],
+  content: ['./src/components/**/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
