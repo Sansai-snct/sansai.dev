db2c2194951dd48794c2bbdecd5c135a793a6a7c
sansai-SNCT <syunzo1115@icloud.com>
2022-07-09 15:25:10 +0900

    [Update] iconに置き換え

--- src/components/common/hamburger/hamburger.tsx
+++ src/components/common/hamburger/hamburger.tsx
@@ -12,14 +12,25 @@ export const Menu: FC = () => {
   return (
     <nav className="flex">
       <div className="">
-        <button
-          className="bg-gray-400"
-          onClick={() => {
-            handleChange()
-          }}
-        >
-          ボタン
-        </button>
+        <div>
+          <button
+            className="bg-gray-400 "
+            onClick={() => {
+              handleChange()
+            }}
+          >
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M4 6h16M4 12h16M4 18h16" />
+            </svg>
+          </button>
+        </div>
+
         {isOpen ? (
           <ul className="md:hidden">
             <li>
